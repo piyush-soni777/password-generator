f12ee99d8df521711e6b4a9d3c8f1145522faa7c
PIYUSH SONI 🕸 <piyushsoni7777777@gmail.com>
2026-04-20 22:34:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,6 @@ else:
   if len(password) <= 6:
     print("Your password is weak, try to include at least 8 characters for a stronger password.") #if the password weak its present
   elif len(password) == 7:
-    print("Your password is medium, try to include at least 8 characters for a stronger password.")
+    print("Your password is medium, try to include at least 8 characters for a stronger password.")  #if the password medium its present
   else:
     print("Your password is strong.")
